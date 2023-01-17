6e87c915a7a76f2420e033029830f88313f53221
satake08 <117998354+satake08@users.noreply.github.com>
2023-01-17 16:52:35 +0000

    [add]5-10学習

---
+++ db/migrate/20230117162005_create_post_images.rb
@@ -0,0 +1,9 @@
+class CreatePostImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_images do |t|
+      t.string :shop_name
+      t.string :caption
+      t.timestamps
+    end
+  end
+end
